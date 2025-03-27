444c48a13ecff93662d59126252c72e8c67952d0
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2025-03-27 17:58:29 +0600

    add custom hook for Google authentication

---
+++ src/features/auth/useGoggleAuth.js
@@ -0,0 +1,23 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "../../api/axios";
+import { googleLogin } from "./authSlice";
+import { useDispatch } from "react-redux";
+
+const useGoogleAuth = () => {
+    const dispatch = useDispatch();
+  return useGoogleLogin({
+    onSuccess: async ({ code }) => {
+      try {
+        await dispatch(googleLogin(code)).unwrap();
+      } catch (error) {
+        console.error("Google login failed:", error);
+      }
+    },
+    onError: (error) => {
+      console.error("Google login error:", error);
+    },
+    flow: "auth-code",
+  });
+};
+
+export default useGoogleAuth;
